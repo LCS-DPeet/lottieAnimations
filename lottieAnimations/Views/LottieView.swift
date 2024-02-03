b05d67baa09a39477a9cf4a82b3ff0a6a82a44b6
Danika Peet <DPeet@lcs.on.ca>
2024-02-03 16:24:27 -0500

    Added several animations that I like.

--- lottieAnimations/Views/LottieView.swift
+++ lottieAnimations/Views/LottieView.swift
@@ -1,18 +1,52 @@
 //
 //  LottieView.swift
-//  lottieAnimations
+//  LottieAnimationsTest
 //
-//  Created by Danika Peet on 2024-02-03.
+//  Created by Russell Gordon on 2023-01-27.
 //
 
+import UIKit
 import SwiftUI
+import Lottie
 
-struct LottieView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct LottieView: UIViewRepresentable {
+
+    @State var animationNamed: String
+
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        
+        let view = UIView(frame: .zero)
+        
+        // Lottie View
+        let animationView = LottieAnimationView()
+        let animation = LottieAnimation.named(animationNamed)
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+        
+        // Constraints
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+        
+        return view
+        
     }
-}
-
-#Preview {
-    LottieView()
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+    
+    typealias UIViewType = UIView
+    
+    struct LottieView_Previews: PreviewProvider {
+        static var previews: some View {
+            LottieView(animationNamed: "782-check-mark-success")
+        }
+    }
 }
