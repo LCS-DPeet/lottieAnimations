f596cc9f27f98e94d6cd1b5832014fbd5ea16c9d
Danika Peet <DPeet@lcs.on.ca>
2024-02-03 16:20:58 -0500

    Added Mr. Gordon's helper view.

---
+++ lottieAnimations/Views/LottieView.swift
@@ -0,0 +1,18 @@
+//
+//  LottieView.swift
+//  lottieAnimations
+//
+//  Created by Danika Peet on 2024-02-03.
+//
+
+import SwiftUI
+
+struct LottieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LottieView()
+}
